e587560f58bf72ade99555b648c097fddd585edc
Alkariz <70Ru.Alkariz@gmail.com>
2020-11-22 15:43:35 +0700

    Добавление 5 проектов, зависимостей между ними, тестов. Расширен тест в проекте common для проверки корректного создания объекта из JSON.

---
+++ common/src/test/java/DTO/PointDTOTest.java
@@ -0,0 +1,37 @@
+package DTO;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jdev on 06.03.2017.
+ */
+public class PointDTOTest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String autoId = "o567gfd";
+    private Double lat = 56.0;
+    private Double lon = 74.0;
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId, dto.getAutoId());
+        assertTrue(dto.getLat()==lat);
+        assertTrue(dto.getLon()==lon);
+    }
+}
